8434b70a71f10a200d6679b5df2b5a3361ba0fe9
zstg <o0vckutt@duck.com>
2025-09-28 23:22:20 +0530

    Added Cosmic DE

--- flake.nix
+++ flake.nix
@@ -26,73 +26,87 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, sddm-astronaut-theme, stylix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, zen-browser, sddm-astronaut-theme, stylix, ... } @ inputs:
     let
       system = "x86_64-linux";
       username = "stig";
+      # Common modules shared across all ISOs
+      commonModules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
+        ./hosts/common
+        sddm-astronaut-theme.nixosModules.sddm-theme
+        home-manager.nixosModules.home-manager
+        stylix.nixosModules.stylix
+        {
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "/tmp/${toString self.lastModified}.bak";
+          home-manager.users.${username} = import ./home/linux.nix;
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+            system = "x86_64-linux";
+          };
+        }
+        ({ lib, pkgs, ... }: {
+          boot.supportedFilesystems.zfs = lib.mkForce false;
+          networking.networkmanager.enable = lib.mkForce true;
+          environment.systemPackages = [
+            zen-browser.packages.${system}.default
+          ];
+        })
+      ];
     in {
       packages.${system} = {
-        # default = self.nixosConfigurations.CosmOS-Hyprland.config.system.build.isoImage;
-        CosmOS-GNOME = self.nixosConfigurations.CosmOS-GNOME.config.system.build.isoImage;
+        # default = self.nixosConfigurations.CosmOS-Cosmic.config.system.build.isoImage;
+        CosmOS-Cosmic = self.nixosConfigurations.CosmOS-Cosmic.config.system.build.isoImage;
         CosmOS-Hyprland = self.nixosConfigurations.CosmOS-Hyprland.config.system.build.isoImage;
+        CosmOS-Niri = self.nixosConfigurations.CosmOS-Niri.config.system.build.isoImage;
+        CosmOS-GNOME = self.nixosConfigurations.CosmOS-GNOME.config.system.build.isoImage;
       };
 
       nixosConfigurations = {
+        CosmOS-Cosmic = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs username; };
+          modules = [
+            ./hosts/CosmOS-Cosmic
+            ({ }: {
+              isoImage.edition = "cosmic";
+            })
+          ] ++ commonModules;
+        };
         CosmOS-Hyprland = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs username; };
           modules = [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
             ./hosts/WM-spins
-            sddm-astronaut-theme.nixosModules.sddm-theme
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "/tmp/${toString self.lastModified}.bak";
-              home-manager.users.${username} = import ./home/linux.nix;
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                system = "x86_64-linux";
-              };
-            }
-            ({ lib, pkgs, ... }: {
-              boot.supportedFilesystems.zfs = lib.mkForce false;
-              networking.networkmanager.enable = lib.mkForce true;
-
-              environment.systemPackages = [
-                zen-browser.packages.${system}.default
-              ];
+            ({ lib, ... }: {
+              isoImage.edition = "hyprland";
+              options.hyprland.enabled = lib.mkForce true;
+              options.niri.enabled = lib.mkForce false;
             })
-            stylix.nixosModules.stylix
-          ];
+          ] ++ commonModules;
         };
-
+        CosmOS-Niri = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs username; };
+          modules = [
+            ./hosts/WM-spins
+            ({ lib, ... }: {
+              isoImage.edition = "niri";
+              options.hyprland.enabled = lib.mkForce false;
+              options.niri.enabled = lib.mkForce true;
+            })
+          ] ++ commonModules;
+        };
         CosmOS-GNOME = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs username; };
           modules = [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
             ./hosts/CosmOS-GNOME
-            stylix.nixosModules.stylix
-            {
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "/tmp/${toString self.lastModified}.bak";
-              home-manager.users.${username} = import ./home/linux.nix;
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                system = "x86_64-linux";
-              };
-            }
-            ({ lib, pkgs, ... }: {
+            ({ }: {
               isoImage.edition = "gnome";
-              boot.supportedFilesystems.zfs = lib.mkForce false;
-              networking.networkmanager.enable = lib.mkForce true;
-
-              environment.systemPackages = [
-                zen-browser.packages.${system}.default
-              ];
             })
-          ];
+          ] ++ commonModules;
         };
       };
     };
---
+++ hosts/CosmOS-Cosmic/default.nix
@@ -0,0 +1,30 @@
+{pkgs, lib, ...}: {
+  imports = [ ../common ];
+  services.displayManager.cosmic-greeter.enable = false;
+  services.desktopManager.cosmic = {
+    enable = true;
+    xwayland.enable = true;
+    showExcludedPkgsWarning = false;
+  };
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-term
+    cosmic-edit
+    cosmic-store
+    cosmic-randr
+    cosmic-reader
+    # cosmic-player
+    # cosmic-greeter # this is required for the lock screen
+    cosmic-ext-ctl
+    cosmic-files
+    kdePackages.xwaylandvideobridge
+  ];
+
+  home-manager.users.stig.home = {
+    activation.cleanupCosmic = lib.mkBefore ''
+      rm -rf ~/.config/cosmic
+    '';
+    activation.configureCosmic = lib.mkAfter ''
+      ln -sf ~/.nixos/hosts/Krypton/services/desktopmanager/cosmic/ ~/.config/
+    '';
+  };
+}
